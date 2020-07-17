cfd2892c0d53de6c4439361faf109df5ae5a90b6
JoshWhite <jwhite927@gmail.com>
2020-07-17 12:30:35 -0400

    Changed curl file output name to temp.zip

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -o data.zip -L https://video.udacity-data.com/topher/2018/March/5aab379c_bikeshare-2/bikeshare-2.zip
-unzip bikeshare.zip chicago.csv new_york_city.csv washington.csv
-rm data.zip
+curl -o temp.zip -L 'https://video.udacity-data.com/topher/2018/March/5aab379c_bikeshare-2/bikeshare-2.zip'
+unzip temp.zip chicago.csv new_york_city.csv washington.csv
+rm temp.zip
 
