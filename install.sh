4d3c0a03c29a15b7297a7832957aa564cbc78e8f
JoshWhite <jwhite927@gmail.com>
2020-07-21 05:21:13 -0400

    docs: Add comments to data download script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Download bikeshare archive from udacity
 curl -o temp.zip -L 'https://video.udacity-data.com/topher/2018/March/5aab379c_bikeshare-2/bikeshare-2.zip'
+
+# Unzip only the relevant .csv files from the archive
 unzip temp.zip chicago.csv new_york_city.csv washington.csv
+
+# Delete the archive when finished
 rm temp.zip
 
+# Notify user that files have been downloaded
